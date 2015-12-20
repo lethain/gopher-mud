8e51d07a9e50ad2f569ceec77fdfd2f6a3d798f8
Will Larson <lethain@gmail.com>
2015-12-20 10:36:47 -0800

    Refactor more into mud file

--- main.go
+++ main.go
@@ -1,48 +1,14 @@
 package main
 
 import (
-	"github.com/lethain/gopher-mud/telnet"
-	"github.com/lethain/gopher-mud/player"	
-	"bufio"
-	"strings"
-	"log"
-	"net"
+	"github.com/lethain/gopher-mud/mud"
 	"flag"
 )
 
 var loc = flag.String("loc", ":9000", "location:port to run server")
 
-
-func handleConn(conn net.Conn) {
-	defer conn.Close()
-	p := player.Player{Conn: conn}
-	log.Printf("new connection from %v", p)
-
-	conn.Write([]byte(p.Splash()))
-	for {
-		msg, err := bufio.NewReader(conn).ReadString('\n')
-		if err != nil {
-			log.Printf("error reading line: %v", err)
-			continue
-		}
-		msg = strings.Trim(msg, "\n\r\t ")
-		resp, err := p.HandleMessage(msg)
-
-		// always end with newline
-		if !strings.HasSuffix(resp, "\n") {
-			resp += "\n"
-		}
-		
-		conn.Write([]byte(resp))
-		if err != nil {
-			log.Printf("error handling message: %v", err)
-			return
-		}
-	}
-}
-
 func main() {
 	flag.Parse()
-	telnet := telnet.TelnetServer{Loc: *loc, Handler: handleConn}
-	telnet.ListenAndServe()
+	ms := mud.MudServer{Loc: *loc}
+	ms.ListenAndServe()
 }
---
+++ mud/mud.go
@@ -0,0 +1,50 @@
+package mud
+
+import (
+	"bufio"
+	"strings"
+	"log"
+	"net"
+	"github.com/lethain/gopher-mud/telnet"
+	"github.com/lethain/gopher-mud/player"
+)
+
+
+type MudServer struct {
+	Loc string
+	TelnetServer *telnet.TelnetServer
+}
+
+func (ms *MudServer) ListenAndServe() {
+	telnet := telnet.TelnetServer{Loc: ms.Loc, Handler: ms.HandleConn}
+	ms.TelnetServer = &telnet
+	telnet.ListenAndServe()
+}
+
+func (ms *MudServer)  HandleConn(conn net.Conn) {
+	defer conn.Close()
+	p := player.Player{Conn: conn}
+	log.Printf("new connection from %v", p)
+
+	conn.Write([]byte(p.Splash()))
+	for {
+		msg, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			log.Printf("error reading line: %v", err)
+			continue
+		}
+		msg = strings.Trim(msg, "\n\r\t ")
+		resp, err := p.HandleMessage(msg)
+
+		// always end with newline
+		if !strings.HasSuffix(resp, "\n") {
+			resp += "\n"
+		}
+		
+		conn.Write([]byte(resp))
+		if err != nil {
+			log.Printf("error handling message: %v", err)
+			return
+		}
+	}
+}
