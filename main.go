353660a7d385f1c600e1c144b65f74f255e70936
Will Larson <lethain@gmail.com>
2015-12-20 09:50:10 -0800

    Initial

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"log"
+	"strings"
+	"bufio"
+)
+
+
+type TelnetServer struct {
+	Loc string
+	Listener net.Listener
+	Handler func(net.Conn)
+}
+
+func (t *TelnetServer) ListenAndServe() {
+	log.Printf("Starting TelnetServer on %v", t.Loc)
+	ln, err := net.Listen("tcp", t.Loc)
+	if err != nil {
+		panic(err)
+	}
+	t.Listener = ln
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("error accepting connection: %v", err)
+			continue
+		}
+		go t.Handler(conn)
+	}
+
+	select {}
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	log.Printf("handleConn: %v", conn)
+	for {
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			log.Printf("error reading line: %v", err)
+			continue
+		}
+		if message == "exit" {
+			return
+		}
+		log.Printf("Message Received (len %v): '%v'", len(message), message)
+
+		newmessage := strings.ToUpper(message)
+		conn.Write([]byte(fmt.Sprintf("(%v)\t%v\n", len(message), newmessage)))
+	}
+
+}
+
+func main() {
+	telnet := TelnetServer{Loc:":9000", Handler: handleConn}
+	telnet.ListenAndServe()
+}
